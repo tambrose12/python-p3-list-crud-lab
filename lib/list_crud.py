c03f3066e31183b580425c6490dc0a0f9e06a984
Taylor Ambrose <taykesby@gmail.com>
2023-03-19 12:55:59 -0400

    lab complete

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,41 @@
 def create_an_empty_list():
-    return None
+    return []
+
 
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]
+
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
+
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    return l
+
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
+
 
 def remove_element_from_start_of_list(l):
-    return None
+    del l[0]
+    return l
+
 
 def retrieve_first_element_from_list(l):
-    return None
+    first_element = l[0]
+    return first_element
+
 
 def retrieve_element_from_index(l, index):
-    return None
+    retrieved_element = l[index]
+    return retrieved_element
+
 
 def retrieve_last_element_from_list(l):
-    return None
+    last_element = l[-1]
+    return last_element
